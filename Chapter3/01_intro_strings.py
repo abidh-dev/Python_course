dffacd793e2fb163c33212c257f23542a124a82e
Engineer AbidH <abidali70sb@gmail.com>
2025-10-18 01:00:16 +0200

    Strings and Methods

---
+++ Chapter3/01_intro_strings.py
@@ -0,0 +1,4 @@
+first_name = "Abid"
+last_name = "Hussain"
+full_name = first_name + " " + last_name # Concatenate first and last names with a space in between
+print(full_name)  # Output: Abid Hussain
