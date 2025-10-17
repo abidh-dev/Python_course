38a60ba49c1bfa8f1c9925a8225be15738b4f973
Engineer AbidH <abidali70sb@gmail.com>
2025-10-18 00:44:02 +0200

    Variables and Data Types

---
+++ Chapter2/01_variables.py
@@ -0,0 +1,8 @@
+a = 1
+b = 3.14
+c = "Hello, World!"
+d = True
+print(type(a))  # Output: <class 'int'>
+print(type(b))  # Output: <class 'float'>
+print(type(c))  # Output: <class 'str'>
+print(type(d))  # Output: <class 'bool'>
---
+++ Chapter2/02_operators.py
@@ -0,0 +1,19 @@
+# assigment operators
+x = 10
+y = 5
+x += y  # x = x + y
+print("After += :", x)  # Output: 15
+x -= y  # x = x - y
+print("After -= :", x)  # Output: 10
+x *= y  # x = x * y
+print("After *= :", x)  # Output: 50
+
+# comparison operators
+a = 10
+b = 20
+print("a == b :", a == b)  # Output: False
+print("a != b :", a != b)  # Output: True
+print("a > b  :", a > b)   # Output: False
+print("a < b  :", a < b)   # Output: True
+print("a >= b :", a >= b)  # Output: False
+print("a <= b :", a <= b)  # Output: True
